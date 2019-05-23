5b5a7b7ed3dcd5106cb164a3489846868de8350b
DESKTOP-FE3E8CL-JeeFo <dochioiu.alex@gmail.com>
2019-05-23 19:20:58 +0100

    Template finished

--- app/src/main/java/com/github/alexdochioiu/core/EntryActivity.kt
+++ app/src/main/java/com/github/alexdochioiu/core/EntryActivity.kt
@@ -2,6 +2,7 @@ package com.github.alexdochioiu.core
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.github.alexdochioiu.core.navigation.Features
 
 internal class EntryActivity : AppCompatActivity() {
@@ -9,7 +10,15 @@ internal class EntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //setContentView(R.layout.activity_entry)
+        //TODO this can be a splash activity
+
+        //approach 1 : In my view it is better in this particular case since we do not need to inflate a layout
+        //                 and it also allows us to better manage the flow between dynamic features provided on demand
         navigateToFeature(Features.Main)
+
+
+        //approach 2 : Using the new navigation component
+        //setContentView(R.layout.activity_entry)
+        //findNavController(R.id.nav_host).navigate(R.id.navigation_main_feature)
     }
 }
