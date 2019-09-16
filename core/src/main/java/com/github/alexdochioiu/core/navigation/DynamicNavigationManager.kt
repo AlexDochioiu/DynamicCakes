02236a718f9a7cb98179dd874d1bc02bf4c2b468
Alexandru Dochioiu <alex.jeefo@gmail.com>
2019-09-16 11:12:07 +0100

    Clean Code
    * Updated target and compile sdk to 29
    * Removed some navigation code/layout file

--- core/src/main/java/com/github/alexdochioiu/core/navigation/DynamicNavigationManager.kt
+++ core/src/main/java/com/github/alexdochioiu/core/navigation/DynamicNavigationManager.kt
@@ -21,17 +21,20 @@ import android.content.Intent
 import com.github.alexdochioiu.core.di.CoreScope
 import javax.inject.Inject
 
+/**
+ * TODO this class is very basic at the moment but if we start having features provided on-demand and features which
+ *       require args, considerably more work should be done
+ */
 @CoreScope
 class DynamicNavigationManager @Inject internal constructor() {
 
-    //TODO this class is very basic at the moment but if we start having features provided on-demand and features which
-    //TODO    require args, considerably more work should be done
     fun navigateToFeature(activity: Activity, feature: DynamicFeature, flags: Int = 0) {
         activity.startActivity(feature.makeNewIntent(flags))
     }
 
+    companion object {
+        private fun DynamicFeature.makeNewIntent(flags: Int): Intent =
+            Intent(Intent.ACTION_VIEW).setClassName(this.entryPackageName, this.entryActivityName)
+                .apply { addFlags(flags) }
+    }
 }
-
-private fun DynamicFeature.makeNewIntent(flags: Int): Intent =
-    Intent(Intent.ACTION_VIEW).setClassName(this.entryPackageName, this.entryActivityName)
-        .apply { addFlags(flags) }
